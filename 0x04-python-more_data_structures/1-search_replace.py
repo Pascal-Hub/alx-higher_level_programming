910a17ff0e919464d457dfe883184e7ca7f4a1d8
Pascal-Hub <sequencepascal@gmail.com>
2023-01-12 05:58:09 -0800

    python-more-data-structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
-    """Replace elements in a given index number"""
-
-    new_list = []
-
-    for i in my_list:
-        if i == search:
-            new_list.append(replace)
-        else:
-            new_list.append(i)
-
-    return new_list 
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list) 
